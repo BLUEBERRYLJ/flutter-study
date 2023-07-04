2c3db479212dca53aeeda21ee1d94311350ba837
Hyeonwoo <leojung0909@gmail.com>
2023-07-04 17:09:46 +0900

    initial commit

---
+++ android/app/src/main/kotlin/kr/smartdoctor/study/flutter_lab/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.smartdoctor.study.flutter_lab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ch2/test.dart
@@ -0,0 +1,26 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Test'),
+        ),
+        body: Center(
+          child: Text(wordPair.first),
+        ),
+      )
+    );
+  }
+
+}
